53fae781ed4a28390fee7fe2496d28bfcf1c5b06
devjrios <159106238+devjrios@users.noreply.github.com>
2026-08-02 03:04:44 -0500

    optional value for field

--- src/models/seguimiento_docente_asignatura.py
+++ src/models/seguimiento_docente_asignatura.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 class SeguimientoDocenteAsignatura(BaseModel):
@@ -16,7 +16,7 @@ class SeguimientoDocenteAsignatura(BaseModel):
     Comentarios_Estables: int = Field(..., alias='Comentarios Estables')
     Comentarios_En_Riesgo: int = Field(..., alias='Comentarios En Riesgo')
     Comentarios_En_Mejora: int = Field(..., alias='Comentarios En Mejora')
-    Semestres_Desde_Ultima_Calificación: int = Field(
+    Semestres_Desde_Ultima_Calificación: Optional[int] = Field(
         ..., alias='Semestres Desde Ultima Calificación'
     )
     Reingreso: bool
